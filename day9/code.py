8d7df629699e29ba80ec428b0ce54978991ac80d
Abhiroop25902 <abhiroop.m25902@gmail.com>
2021-12-09 12:49:39 +0530

    [NEW] done day9

---
+++ day9/code.py
@@ -0,0 +1,36 @@
+from functools import reduce
+
+FILE_NAME = 'input.txt'
+#up, right, down, left
+DX = (0, 1, 0, -1)
+DY = (-1, 0, 1, 0)
+
+
+def findBasinSize(heightMap, i, j, visited):
+    # print(visited)
+    visited.add((i,j))
+    for dx, dy in zip(DX, DY):
+        if (i + dy >= 0) and (i + dy < len(heightMap)) and (j + dx >= 0) and (j + dx < len(row)) and (heightMap[i+dy][j+dx] != 9) and ((i+dy, j+dx) not in visited):
+            findBasinSize(heightMap, i+dy, j+dx, visited)
+
+basinSizes = []
+
+heightMap = [[int(char) for char in line.rstrip('\n')]
+             for line in open(FILE_NAME)]
+
+for i, row in enumerate(heightMap):
+    for j, elem in enumerate(row):
+        isValley = True
+        for dx, dy in zip(DX, DY):
+            if (i + dy >= 0) and (i + dy < len(heightMap)) and (j + dx >= 0) and (j + dx < len(row)) and (elem >= heightMap[i+dy][j+dx]):
+                isValley = False
+                break
+        if isValley == True:
+            visited = set()
+            findBasinSize(heightMap, i, j, visited)
+            basinSizes.append(len(visited))
+            basinSizes.sort(reverse=True)
+            if len(basinSizes) == 4:
+                basinSizes.pop()
+
+print(reduce(lambda x,y : x*y , basinSizes))
