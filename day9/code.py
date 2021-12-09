cd3c310e26d4b93e184b20afb5b15686034fe30c
Abhiroop25902 <abhiroop.m25902@gmail.com>
2021-12-09 23:28:08 +0530

    [EDIT] code compaction

--- day9/code.py
+++ day9/code.py
@@ -8,10 +8,11 @@ DY = (-1, 0, 1, 0)
 
 def findBasinSize(heightMap, i, j, visited):
     # print(visited)
-    visited.add((i,j))
+    visited.add((i, j))
     for dx, dy in zip(DX, DY):
         if (i + dy >= 0) and (i + dy < len(heightMap)) and (j + dx >= 0) and (j + dx < len(row)) and (heightMap[i+dy][j+dx] != 9) and ((i+dy, j+dx) not in visited):
             findBasinSize(heightMap, i+dy, j+dx, visited)
+
 
 basinSizes = []
 
@@ -22,7 +23,7 @@ for i, row in enumerate(heightMap):
     for j, elem in enumerate(row):
         isValley = True
         for dx, dy in zip(DX, DY):
-            if (i + dy >= 0) and (i + dy < len(heightMap)) and (j + dx >= 0) and (j + dx < len(row)) and (elem >= heightMap[i+dy][j+dx]):
+            if (0 <= i + dy < len(heightMap)) and (0 <= j + dx < len(row)) and (elem >= heightMap[i+dy][j+dx]):
                 isValley = False
                 break
         if isValley == True:
@@ -33,4 +34,4 @@ for i, row in enumerate(heightMap):
             if len(basinSizes) == 4:
                 basinSizes.pop()
 
-print(reduce(lambda x,y : x*y , basinSizes))
+print(reduce(lambda x, y: x*y, basinSizes))
